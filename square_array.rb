ff029ea57751e81db3cc9133cc36fb21ee66a26a
Ben Parish <parishben13@gmail.com>
2020-05-07 03:17:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   #squared_it = []
   #array.each { |element| squared_it << element ** 2 }
   #squared_it
-sap=array.collect{|num| num**2}
-puts sap.inspect
+array.collect{|num| num**2}
+puts array.inspect
 end
 
 square_array(array)
